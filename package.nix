51b7ed04ad40edb3329a6f4197d7546ba01ffb50
Bart Oostveen <bart@bartoostveen.nl>
2026-08-16 23:15:14 +0200

    chore(docs): fixup description

--- package.nix
+++ package.nix
@@ -13,7 +13,7 @@ buildGoModule (finalAttrs: {
   vendorHash = "sha256-7p1Pvy1TKZQZ/GBVATbryq6s+kcaK7A84kEQuw1x8os=";
 
   meta = {
-    description = "Simple, but beatiful Matrix webhook handler for cPanel notifications";
+    description = "Simple Matrix webhook handler for cPanel notifications";
     homepage = "https://git.bartoostveen.nl/bart/cpanel-matrix";
     license = lib.licenses.gpl3Only;
     mainProgram = "cpanel-matrix";
