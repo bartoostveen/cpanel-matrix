eb9e1919bb290c38449c89eb722bdc11889a7ece
Bart Oostveen <bart@bartoostveen.nl>
2026-08-16 23:17:56 +0200

    chore(nix): add changelog, maintainers, platforms

--- package.nix
+++ package.nix
@@ -15,7 +15,10 @@ buildGoModule (finalAttrs: {
   meta = {
     description = "Simple Matrix webhook handler for cPanel notifications";
     homepage = "https://git.bartoostveen.nl/bart/cpanel-matrix";
+    changelog = "https://git.bartoostveen.nl/bart/cpanel-matrix/src/tag/v${finalAttrs.version}/CHANGELOG.md";
     license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ bartoostveen ];
     mainProgram = "cpanel-matrix";
+    platforms = lib.platforms.all;
   };
 })
